2c2cef2faaf22098dd350f3e38347a5189df862f
Dmitry Tantsur <dtantsur@redhat.com>
2015-07-27 16:35:09 +0200

    Prepare to run on openshift

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='ironic-bug-dashboard',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      author='Dmitry Tantsur',
+      url='http://ironic-divius.rhcloud.com/',
      )
