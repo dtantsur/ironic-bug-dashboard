efebfc6e15fbc5767b12a62def6101058cc39eb2
Dmitry Tantsur <divius.inside@gmail.com>
2017-08-25 11:47:00 +0200

    Get rid of OpenShift 2 remainings

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 
-import os
-
-
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'),
-         dict(__file__=virtualenv))
-except IOError:
-    pass
-
-
 from ironic_bug_dashboard import app as application  # noqa
+
+
+if __name__ == '__main__':
+    application.run()
