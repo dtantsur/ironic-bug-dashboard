7b7b8677673abb042353a51af65220494f3e06e8
Dmitry Tantsur <dtantsur@protonmail.com>
2023-11-13 16:25:49 +0100

    Remove any traces of OpenShift

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-worker_class = 'eventlet'
-
-forwarded_allow_ips = '*'
-secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import ironic_bug_dashboard
-
-
-application = ironic_bug_dashboard.app
-
-if __name__ == '__main__':
-    ironic_bug_dashboard.main()
