35b39059629a8ca92763238c3a5f1757c97cf35f
Dmitry Tantsur <dtantsur@protonmail.com>
2023-11-13 19:47:53 +0100

    Start up to 4 workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,4 +3,5 @@ import multiprocessing
 accesslog = "-"
 bind = "0.0.0.0:8000"
 proc_name = "ironic-bug-dashboard"
+workers = min(multiprocessing.cpu_count() + 1, 4)
 worker_class = "aiohttp.GunicornWebWorker"
